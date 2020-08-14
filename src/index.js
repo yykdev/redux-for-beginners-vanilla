ac5715beb01c8729d92f4fe5824a02bf7f11ee97
yykdev <assavictory@gmail.com>
2020-08-14 17:23:34 +0900

    reducer 란?

--- src/index.js
+++ src/index.js
@@ -4,21 +4,9 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerHTML = count;
-
-const updateText = () => {
-    number.innerHTML = count;
+const countModifier = (count = 0) => {
+    return count;
 };
+const countStore = createStore(countModifier);
 
-const handleAdd = () => {
-    count = count + 1;
-    updateText();
-};
-const handleMinus = () => {
-    count = count - 1;
-    updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
