feacc8896e8159a2b9a42a10c2c59b082d58523f
yykdev <assavictory@gmail.com>
2020-08-14 18:47:26 +0900

    redux/reducer - Actions/Subscriptions

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import { createStore } from "redux";
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+number.innerText = 0;
 
-const countModifier = (count = 0) => {
-    return count;
+const countModifier = (count = 0, action) => {
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
 const countStore = createStore(countModifier);
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
 
-console.log(countStore.getState());
+const handleAdd = () => {
+    countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+    countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
